4e7f582080fdb174f6378853c58c4eb51f20bce7
EyalEliav <eyaleliav15@gmail.com>
2023-06-19 23:40:59 +0300

    add graph drawer API

---
+++ function_graph_draw.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class GraphDrawer:
+    def __init__(self, func, graph_range):
+        x = np.linspace(-graph_range, graph_range, 100)
+        y = np.linspace(-graph_range, graph_range, 100)
+        X, Y = np.meshgrid(x, y)
+        Z = func(X, Y)
+        fig = plt.figure()
+        self.ax = fig.add_subplot(111, projection='3d')
+        self.ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.7)
+        self.ax.set_xlabel('X')
+        self.ax.set_ylabel('Y')
+        self.ax.set_zlabel('Z')
+        self.ax.set_title(func.__name__)
+
+    def draw_point(self, point):
+        self.ax.scatter(point[0], point[1], point[2], color='black')
+
+    def finish_draw(self):
+        plt.show()
