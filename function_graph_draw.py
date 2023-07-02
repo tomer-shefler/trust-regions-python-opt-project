0f9df820373843442c64e0194573fbcaafbacc7f
EyalEliav <eyaleliav15@gmail.com>
2023-07-02 22:33:05 +0300

    fix some code

--- function_graph_draw.py
+++ function_graph_draw.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 
 class GraphDrawer:
@@ -7,18 +8,23 @@ class GraphDrawer:
         x = np.linspace(-graph_range, graph_range, 100)
         y = np.linspace(-graph_range, graph_range, 100)
         X, Y = np.meshgrid(x, y)
-        Z, _, _ = func(np.array([X, Y]))
+        Z = np.zeros(([len(x), len(y)]))
+
+        for i in range(0, len(x)):
+            for j in range(0, len(y)):
+                Z[j, i], _, _ = func(np.array([x[i], y[j]]))
         fig = plt.figure()
-        self.ax = fig.add_subplot(111, projection='3d')
-        self.ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.7)
-        self.ax.set_xlabel('X')
-        self.ax.set_ylabel('Y')
-        self.ax.set_zlabel('Z')
-        self.ax.set_title(func.__name__)
+        self.ax = fig.add_subplot(111, projection="3d")
+        self.ax.plot_surface(X, Y, Z, cmap="viridis", alpha=0.7)
+        self.ax.set_xlabel("X")
+        self.ax.set_ylabel("Y")
+        self.ax.set_zlabel("Z")
+        self.ax.set_title(f"trust region - {func.__name__}")
+        self.func = func
 
     def draw_point(self, point):
-        self.ax.scatter(point[0], point[1], point[2], color='black')
+        self.ax.scatter(point[0], point[1], point[2], color="black")
 
     def finish_draw(self):
+        plt.savefig(os.path.dirname(__file__) + f"/trust region - {self.func.__name__}")
         plt.show()
-        
--- trust_region_dogleg.py
+++ trust_region_dogleg.py
@@ -2,22 +2,25 @@ import numpy as np
 import examples
 import function_graph_draw
 
+
 class TrustRegion(object):
-    def __init__(self, eta=0.15, tol=1e-4, max_trust_radius=1.0):
+    def __init__(self, eta=0.15, tol=1e-4, max_trust_radius=1, max_iter=100000):
         self.eta = eta
         self.tol = tol
-        self.max_trust_radius = 1.0
-        
+        self.max_trust_radius = max_trust_radius
+        self.max_iter = max_iter
 
     def dogleg(self, g, B, trust_radius):
         # full newton step lies inside the trust region
-        pb = -np.linalg.inv(B)@g
-        if np.linalg.norm(pb) <= trust_radius:
+        pb = 0
+        if np.count_nonzero(B) > 0:
+            pb = -np.linalg.inv(B) @ g
+        if trust_radius >= np.linalg.norm(pb) > 0:
             return pb
 
-        # step along the steepest descent direction lies outside the  
-        # trust region       
-        pu = - (np.dot(g, g) / np.dot(g, B@g)) * g
+        # step along the steepest descent direction lies outside the
+        # trust region
+        pu = -(np.dot(g, g) / np.dot(g, B @ g)) * g
         dot_pu = np.dot(pu, pu)
         norm_pu = np.sqrt(dot_pu)
         if norm_pu >= trust_radius:
@@ -29,34 +32,35 @@ class TrustRegion(object):
         d = pu_pb_pu_sq ** 2 - pb_pu_sq * (dot_pu - trust_radius ** 2)
         tau = (-pu_pb_pu_sq + np.sqrt(d)) / pb_pu_sq + 1
         if tau < 1:
-            return pu*tau
-        return pu + (tau-1) * pb_pu
+            return pu * tau
+        return pu + (tau - 1) * pb_pu
 
     def trust_region(self, x0, f):
-        
         record = []
-        #initial point
+        # initial point
         x = x0
-        #initial radius
+        # initial radius
         trust_radius = 0.1
         f_x, _, _ = f(x)
         record.append((x, f_x))
-        while True:
+        gk = 0
+        g_prev = 0
+        for _ in range(self.max_iter):
             f_x, g, B = f(x, should_hessian=True)
-            H = np.linalg.inv(B)
+            g_prev = np.linalg.norm(gk)
+            gk = np.linalg.norm(g)
             p = self.dogleg(g, B, trust_radius)
             f_x_p, _, _ = f(x + p)
-            rho = (f_x - f_x_p)/(-(np.dot(g, p) + 0.5 * np.dot(p, B@p)))
+            rho = (f_x - f_x_p) / (-(np.dot(g, p) + 0.5 * np.dot(p, B @ p)))
             norm_p = np.linalg.norm(p)
             if rho < 0.25:
-                trust_radius = 0.25 * norm_p
+                trust_radius = 0.25 * trust_radius
+                if rho < self.eta:
+                    continue
             else:
                 if rho > 0.75 and norm_p == trust_radius:
                     trust_radius = min(2.0 * trust_radius, self.max_trust_radius)
-                else:
-                    trust_radius = trust_radius
-            if rho > self.eta:
-                x = x + p
+            x = x + p
             record.append((x, f_x))
             if np.linalg.norm(g) < self.tol:
                 break
@@ -65,12 +69,11 @@ class TrustRegion(object):
 
 if __name__ == "__main__":
     t = TrustRegion()
-    x0 = np.array([5,5])
+    x0 = np.array([-1, 2])
     l = t.trust_region(x0, examples.rosenbrock)
-    graph_drawer = function_graph_draw.GraphDrawer(examples.rosenbrock, graph_range=5)
+    graph_drawer = function_graph_draw.GraphDrawer(examples.rosenbrock, graph_range=2)
     for slot in l:
         x, f_x = slot
         point = [x[0], x[1], f_x]
         graph_drawer.draw_point(point)
     graph_drawer.finish_draw()
-    
